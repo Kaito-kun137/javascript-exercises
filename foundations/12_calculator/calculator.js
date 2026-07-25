09e7ac69cc7de497249c9466e34a300d59ce2e89
Khetho <133853030+Kaito-kun137@users.noreply.github.com>
2026-07-25 22:36:37 +0200

    Completed exercise 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((product, number) => product * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0 || number === 1) {
+		return 1;
+	}
+
+	let result = 1;
+	for (let i = 2; i <= number; i++) {
+		result *= i;
+	}
+
+	return result;
 };
 
 // Do not edit below this line
