f54ab1c6ac2b8db315d76b807a46182971dada9c
Khetho <133853030+Kaito-kun137@users.noreply.github.com>
2026-07-25 22:40:48 +0200

    Completed exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const normalizedString = string
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, '');
 
+  return normalizedString === normalizedString.split('').reverse().join('');
 };
 
 // Do not edit below this line
