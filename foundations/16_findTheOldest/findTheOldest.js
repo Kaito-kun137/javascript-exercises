a0577a81e60b67af6fc33aad20c681952d5b6e16
Khetho <133853030+Kaito-kun137@users.noreply.github.com>
2026-07-25 22:46:18 +0200

    COmpleted exercise 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((oldestPerson, person) => {
+    const oldestAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
+    const personAge = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
 
+    return personAge > oldestAge ? person : oldestPerson;
+  });
 };
 
 // Do not edit below this line
